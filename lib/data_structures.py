3aa2c8b6236ab69b1dc8d43d1cae0394b39e53bb
Irene Musau <irenemwikalii04@gmail.com>
2024-10-16 08:55:44 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -41,4 +41,5 @@ def get_average_heat_level(spicy_foods):
     return total_heat // len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    
+    spicy_foods.append(spicy_food)
+    return spicy_foods
